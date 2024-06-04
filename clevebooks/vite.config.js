4d25a7fd9d74a3366154890881bbef8d8b9b7780
Himanshu Singh <138711578+himansh025@users.noreply.github.com>
2024-06-04 17:04:49 +0530

    Update vite.config.js

--- clevebooks/vite.config.js
+++ clevebooks/vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// vite.config.js
+export default {
+  base: '/your-subdirectory/', // if your site is not at the root
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
